15e5c528871ad599a56061623bcf9cb41aa33edb
H8sGR8 <mateuszkobalczyk8@gmail.com>
2024-12-30 20:32:25 +0100

    set more precise position

--- pacman.cpp
+++ pacman.cpp
@@ -12,24 +12,28 @@ void Pacman::paintEvent(QPaintEvent* /*event*/){
     QPainter painter(this);
     painter.setPen(Qt::NoPen);
     painter.setBrush(QColor(color));
-    painter.drawPie(QRect(0,0,30,30),0,360*16);
+    painter.drawPie(QRect(0,0,50,50),0,360*16);
 }
 
 void Pacman::keyPressEvent(QKeyEvent *event){
     if(event->key() == Qt::Key_Up){
         if(currentDirection == NO_DIR) currentDirection = UP;
+        if(currentDirection == DOWN) nextDirection = NO_DIR;
         else nextDirection = UP;
     }
     if(event->key() == Qt::Key_Left){
         if(currentDirection == NO_DIR) currentDirection = LEFT;
+        if(currentDirection == RIGHT) nextDirection = NO_DIR;
         else nextDirection = LEFT;
     }
     if(event->key() == Qt::Key_Down){
         if(currentDirection == NO_DIR) currentDirection = DOWN;
+        if(currentDirection == UP) nextDirection = NO_DIR;
         else nextDirection = DOWN;
     }
     if(event->key() == Qt::Key_Right){
         if(currentDirection == NO_DIR) currentDirection = RIGHT;
+        if(currentDirection == LEFT) nextDirection = NO_DIR;
         else nextDirection = RIGHT;
     }
 }
